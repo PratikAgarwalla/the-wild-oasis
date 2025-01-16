6a5ea53bc0e118461fe2d09337f10fa564269796
Pratik Agarwalla <pratikagarwalla2003@gmail.com>
2025-01-16 12:35:16 +0530

    Update supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = "https://ubonnlvxqyiythvnlxmj.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVib25ubHZ4cXlpeXRodm5seG1qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA3NzY2MzAsImV4cCI6MjAzNjM1MjYzMH0.uLh-IUo_jFirWaZ4rnovmGuQ2QSohh3hiR463xSoXmw";
+export const supabaseUrl = import.meta.env.VITE_SUPABSE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
